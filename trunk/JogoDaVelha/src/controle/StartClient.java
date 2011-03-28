393479aced5c88bdf5f4c3608019750ae9f1a3eb
a.souzaecomp071 <a.souzaecomp071@243a267f-158f-8ca8-d25f-888a00007459>
2011-03-28 15:34:44 +0000

    Melhoria da interface da sala de espera e adicão da jlist no scrollpane. Falta ainda adicinar o evento no botão.

--- trunk/JogoDaVelha/src/controle/StartClient.java
+++ trunk/JogoDaVelha/src/controle/StartClient.java
@@ -5,6 +5,10 @@
 
 package controle;
 
+import java.util.ArrayList;
+import modelo.Cliente;
+import visual.SalaDeEspera2;
+
 /**
  *
  * @author andre
@@ -12,10 +16,38 @@ package controle;
 public class StartClient{
 
     public static void main(String[] args) {
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 
-        ControlaCliente controla = new ControlaCliente();
+       Cliente um = new Cliente("um", "192.168.0.0", 2020);
+       Cliente dois = new Cliente("dois", "192.168.0.1", 2020);
+       Cliente tres = new Cliente("tres", "192.168.0.2", 2020);
+       Cliente quatro = new Cliente("quatro", "192.168.0.3", 2020);
+       Cliente cinco = new Cliente("cinco", "192.168.0.3", 2020);
+       Cliente seis = new Cliente("seis", "192.168.0.3", 2020);
+       Cliente sete = new Cliente("sete", "192.168.0.3", 2020);
+       Cliente oito = new Cliente("oito", "192.168.0.3", 2020);
+       Cliente nove = new Cliente("nove", "192.168.0.3", 2020);
+       Cliente dez = new Cliente("dez", "192.168.0.3", 2020);
+
+       clientes.add(um);
+       clientes.add(dois);
+       clientes.add(tres);
+       clientes.add(quatro);
+       clientes.add(cinco);
+       clientes.add(seis);
+       clientes.add(sete);
+       clientes.add(nove);
+       clientes.add(dez);
+       
+       
+        ControlaSalaDeEspera controlaSala = new ControlaSalaDeEspera(clientes);
+        
+        //controlaSala.salaDeEspera.lista.setSelectedIndex(1);
+        System.out.println(controlaSala.salaDeEspera.lista.getSelectedValue());
+        
+        /*ControlaCliente controla = new ControlaCliente();
         Thread t = new Thread(controla);
-        t.start();            
+        t.start();      */
 
     }
 }
