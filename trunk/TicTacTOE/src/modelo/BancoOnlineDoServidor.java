71813eadb488f9463c7029191472b2eb979ba441
douglaseusilva <douglaseusilva@243a267f-158f-8ca8-d25f-888a00007459>
2011-07-20 20:35:55 +0000

    calculo de pontuação computado no servidor, cadastro de usuarios funcionando.

--- trunk/TicTacTOE/src/modelo/BancoOnlineDoServidor.java
+++ trunk/TicTacTOE/src/modelo/BancoOnlineDoServidor.java
@@ -42,14 +42,14 @@ public class BancoOnlineDoServidor {
      * de dados tipo escritas simultâneas.
      * 
      * @param nick
-     * @param addr
-     * @param porta 
+     * @param senha
+     * @param addr 
      */
-    public void cadastroNaLista(String nick, String addr, int porta, int status) {
+    public void cadastroNaLista(String nick, String senha, String addr) {
 
         System.out.println("\n [metodo cadastro na lista] BancoOnlineDoServidor diz: cadastrando " + nick + " na lista");
 
-        Jogador novo = new Jogador(nick, addr, porta, status, 0);
+        Jogador novo = new Jogador(nick, senha, addr);
 
         synchronized (jogadoresCadastrados) {
             jogadoresCadastrados.add(novo);
@@ -60,6 +60,36 @@ public class BancoOnlineDoServidor {
         System.out.println("\n [metodo cadastro na lista] BancoOnlineDoServidor diz:" + jogadoresCadastrados.get(tamanhoDaLista - 1).getNick() + " está cadastrado!");
 
         //retornaLista(receivePacket, nick);
+    }
+
+    /**
+     * Metodo responsável por fazer o login do jogador no servidor. 
+     * O login no servidor consiste na verificação dos dados do usuário, s
+     * e estiverem corretos este método retorna um booelano true
+     * e caso estejam errados ou não existam retora um booelano false.
+     * 
+     * @param nick
+     * @param senha
+     * @return loginValido
+     */
+    public boolean fazerLogin(String nick, String senha) {
+        boolean loginValido = false;
+
+        synchronized (jogadoresCadastrados) {
+
+            for (int i = 0; i < jogadoresCadastrados.size(); i++) {
+
+                if (jogadoresCadastrados.get(i).getNick().equalsIgnoreCase(nick)
+                        && jogadoresCadastrados.get(i).getSenha().equalsIgnoreCase(senha)) {
+                    jogadoresCadastrados.get(i).setStatusON();
+                    loginValido = true;
+                    break;
+                } else {
+                    loginValido = false;
+                }
+            }
+        }
+        return loginValido;
     }
 
     /**
@@ -85,11 +115,14 @@ public class BancoOnlineDoServidor {
      */
     public void atualizaEscore(String nick, String situacao) {
 
+        System.out.println("\n [metodo atualizaEscore] Banco online do Servidor diz: vou atualizar o escore de" + nick);
         synchronized (jogadoresCadastrados) {
 
             for (int i = 0; i < jogadoresCadastrados.size(); i++) {
                 if (jogadoresCadastrados.get(i).getNick().equalsIgnoreCase(nick)) {
+                    System.out.println("\n O Score antigo era:" + jogadoresCadastrados.get(i).getPontuacao());
                     jogadoresCadastrados.get(i).setPontuacao(situacao);
+                    System.out.println("\n O Score novo é:" + jogadoresCadastrados.get(i).getPontuacao());
                 }
             }
 
--- trunk/TicTacTOE/src/modelo/DecodificadorDeAcoesDoCliente.java
+++ trunk/TicTacTOE/src/modelo/DecodificadorDeAcoesDoCliente.java
@@ -54,13 +54,17 @@ public class DecodificadorDeAcoesDoCliente implements Runnable {
                 chamarOUatualizarSalaDeEspera(sentencaMod);
             } else if (split[1].equalsIgnoreCase("TeDesafio")) {
                 recebiConvite(sentencaMod);
-            } else if(split[1].equalsIgnoreCase("AceitoDesafio")){
+            } else if (split[1].equalsIgnoreCase("AceitoDesafio")) {
                 oponenteAceitouDesafio(sentencaMod);
-            } else if(split[1].equalsIgnoreCase("Jogada")){
+            } else if (split[1].equalsIgnoreCase("Jogada")) {
                 jogadaDoOponente(sentencaMod);
+            } else if (split[1].equalsIgnoreCase("RespostaLogin")){
+                confirmacaoDoLogin(split[2]);
+            } else if (split[1].equalsIgnoreCase("CadastroRealizadoComSucesso")){
+                confirmacaoDoCadastro();
             }
 
-            
+
         } else if (tipoDeDado == 1) {
 
             System.out.println(" \n [metodo decodificador] DecodificadorDeAcoesDoCliente diz, recebi uma string seu conteúdo é: " + controle);
@@ -69,15 +73,16 @@ public class DecodificadorDeAcoesDoCliente implements Runnable {
 
             if (split[1].equalsIgnoreCase("Login")) // FAZER LOGIN //
             {
-                efetuarLogin(split[2]);
+                efetuarLogin(split[2], split[3]);
             } else if (split[1].equalsIgnoreCase("enviarConvite")) // ENVIAR CONVITE //
             {
                 desafiarOponente(split[2]); // NA POSIÇÃO 2 DEVE ESTAR O NICK DO ADVERSÁRIO //
-            } else if (split[1].equalsIgnoreCase("enviarMensagem"))
+            }else if (split[1].equalsIgnoreCase("Cadastro")) // ENVIAR CONVITE //
             {
-                enviarMensagem(split[2]);
+                String temp = ":novoCadastro:" + split[2] + ":" + split[3] + ":" + split[4];
+                cadastroNoServidor(temp, split[5]); // NA POSIÇÃO 5 DEVE ESTAR O IP DO SERVER //
             }
-
+               
         }
 
 
@@ -87,11 +92,11 @@ public class DecodificadorDeAcoesDoCliente implements Runnable {
      * Método responsável por enviar as informações do cliente para fazer login no servidor.
      * @param nick 
      */
-    public void efetuarLogin(String nick) {
+    public void efetuarLogin(String nick, String senha) {
 
 
         try {
-            String mensagem = ":" + "Login" + ":" + nick + ":" + StaticControlaJogador.getInstance().getMeuIP();
+            String mensagem = ":" + "Login" + ":" + nick + ":" + senha + ":" + StaticControlaJogador.getInstance().getMeuIP();
 
             System.out.println("\n [metodo cadastro no servidor] Controla Jogador diz: Enviando soliticação de castastro no servidor."); // 10.65.98.134
             Thread fazLogin = new Thread(new EmissorUDP(mensagem, InetAddress.getByName(StaticControlaJogador.getInstance().getIPdoServidor()), 2495));
@@ -101,6 +106,37 @@ public class DecodificadorDeAcoesDoCliente implements Runnable {
             System.out.println("Erro em: ControlaJogador.CadastroNoServidor");
         }
     }
+
+    /**
+     * Este método trata a resposta do servidor quanto a solicitação de login do usuário.
+     * O método esconde a janela de login caso as informações passadas tiverem corretas,
+     * caso contrário, informa ao usuário que seu login ou senha podem estar errados.
+     * 
+     * @param validade 
+     */
+    public void confirmacaoDoLogin(String validade) {
+
+        if (validade.equalsIgnoreCase("Valido")) {
+            StaticControlaJogador.getInstance().escondeJanelaLogin(false);
+        } else if (validade.equalsIgnoreCase("Invalido")) {
+            JOptionPane.showMessageDialog(null, "Seu usuário ou senha nao estão corretos."
+                    + "\n  Tente Novamente", "Login Inválido", 0);
+        }
+
+    }
+    
+    /**
+     * Este método trata a resposta do servidor quanto a solicitação de cadastro do usuário.
+     * O metodo deve informar ao usuário se o cadastro foi realizado com sucesso ou não.
+     * 
+     * 
+     */
+    public void confirmacaoDoCadastro() {
+            
+            JOptionPane.showMessageDialog(null, "Cadastro Realizado Com Sucesso", "Login Inválido", 0);
+
+    }
+    
 
     /**
      *  A função deste método é informar à "sala de espera" a situação dos oponentes, em relação ao seu status atual.
@@ -128,19 +164,19 @@ public class DecodificadorDeAcoesDoCliente implements Runnable {
         int resp = JOptionPane.showConfirmDialog(null, split[2] + " desafiou você para uma partida. ", "Desfio", 0);
 
         if (resp == 0) { // Aceitei o desafio
-            
+
             StaticControlaJogador.getInstance().adicionaNovaPartida(split[2], split[3], false);
             try {
                 String control = ":AceitoDesafio:" + StaticControlaJogador.getInstance().getNick() + ":" + StaticControlaJogador.getInstance().getMeuIP();
                 Thread aceiteiPartida = new Thread(new EmissorUDP(control, InetAddress.getByName(split[3]), 9090));
                 aceiteiPartida.start();
-                
+
             } catch (UnknownHostException ex) {
                 Logger.getLogger(DecodificadorDeAcoesDoCliente.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
+
         } else if (resp == 1) { // Neguei o desafio
-           JOptionPane.showMessageDialog(null,"Eu não aceito o desafio " , "Informação", 1);
+            JOptionPane.showMessageDialog(null, "Eu não aceito o desafio ", "Informação", 1);
         }
 
     }
@@ -151,7 +187,7 @@ public class DecodificadorDeAcoesDoCliente implements Runnable {
      * @param nick
      */
     public void desafiarOponente(String nick) {
-        
+
         System.out.println("[metodo desafiarOponente] DecodificadorDeAcoesDoCliente diz, Vou enviar o convite");
 
         Jogador temp = StaticControlaJogador.getInstance().retornaOponenteDaLista(nick);
@@ -175,19 +211,17 @@ public class DecodificadorDeAcoesDoCliente implements Runnable {
                     + "desafiar jogadores desconectados!", "Informação", 1);
         }
 
-         System.out.println("[metodo desafiarOponente] DecodificadorDeAcoesDoCliente diz, Convite enviado, aguardado resposta...");
-        
+        System.out.println("[metodo desafiarOponente] DecodificadorDeAcoesDoCliente diz, Convite enviado, aguardado resposta...");
+
     }
 
-    
-
-   /**
-    * Se o oponente aceitar o desafio, esse método é chamado para instanciar
-    * uma nova partida para quem fez o desafio.
-    * 
-    * @param sentencaMod
-    */
-    public void oponenteAceitouDesafio(String sentencaMod){
+    /**
+     * Se o oponente aceitar o desafio, esse método é chamado para instanciar
+     * uma nova partida para quem fez o desafio.
+     * 
+     * @param sentencaMod
+     */
+    public void oponenteAceitouDesafio(String sentencaMod) {
 
         String[] split = sentencaMod.split(":");
 
@@ -204,38 +238,15 @@ public class DecodificadorDeAcoesDoCliente implements Runnable {
 
         String[] split = sentencaMod.split(":");
 
-        StaticControlaJogador.getInstance().atualizaJanelaJogo(split[2],split[3]);
+        StaticControlaJogador.getInstance().atualizaJanelaJogo(split[2], split[3]);
     }
 
-    /**
-     * Este método foi desenvolvido para que mensagens de chat fosse enviadas entres clientes
-     * @param nick
-     */
-    private void enviarMensagem(String nick) {
-
-        System.out.println("[metodo enviarMensagem] DecodificadorDeAcoesDoCliente diz, Vou enviar a mensagem de chat");
-
-        Jogador temp = StaticControlaJogador.getInstance().retornaOponenteDaLista(nick);
-
-        String ctrl = ":" + "TeDesafio:" + StaticControlaJogador.getInstance().getNick() + ":" + StaticControlaJogador.getInstance().getMeuIP();
-
-
-        if (temp.getStatus() == 1) {
-            try {
-
-                //InetAddress ip = InetAddress.getByName(temp.getIp());
-
-                Thread envioMensagem = new Thread(new EmissorUDP(ctrl, InetAddress.getByName(temp.getIp()), 9090));
-                envioMensagem.start();
-
-            } catch (UnknownHostException ex) {
-                Logger.getLogger(DecodificadorDeAcoesDoCliente.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } else {
-            JOptionPane.showMessageDialog(null, nick + " está desconectado. \nVocê não pode "
-                    + "desafiar jogadores desconectados!", "Informação", 1);
+    public void cadastroNoServidor(String conteudo, String ipServer) {
+        try {
+            Thread cadastroNoServer = new Thread(new EmissorUDP(conteudo, InetAddress.getByName(ipServer), 2495));
+            cadastroNoServer.start();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(DecodificadorDeAcoesDoCliente.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-         System.out.println("[metodo enviarMensagem] DecodificadorDeAcoesDoCliente diz, Mensagem de chat enviada...");
     }
 }
